1cce5db436c8a58e3f29ccfcd7f011aba1d379a5
TAMILARASI P <tamilarasip.22cse@kongu.edu>
2026-02-13 10:19:23 +0530

    Time: 2 ms (66.51%), Space: 85.2 MB (79.9%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n=nums.length;
+        int f=0;
+        boolean increasing=true;
+        boolean decreasing=true;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]>nums[i+1])
+            {
+                increasing=false;
+            }
+            if(nums[i]<nums[i+1])
+            {
+                decreasing =false;
+            }
+        }
+        
+            return increasing||decreasing;
+        
+    }
+}
